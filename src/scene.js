dae7a490d0e23a0d687ceae433f51ec5f143066d
Ben Coveney <bencoveney@gmail.com>
2019-05-19 20:46:53 +0100

    disgusting optimisations

--- src/scene.js
+++ src/scene.js
@@ -6,7 +6,7 @@ function initScene() {
 }
 
 const gridSize = 16;
-const debug = true;
+const debug = false;
 
 function initCamera() {
   var aspect = window.innerWidth / window.innerHeight;
@@ -21,17 +21,19 @@ function initCamera() {
 
 function initRenderer() {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-  console.log("size", window.innerWidth, window.innerHeight);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   window.document.body.appendChild(renderer.domElement);
   return renderer;
 }
 
+function createColor({ r, g, b }) {
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 const materialCache = {};
 
-function createMaterial({ r, g, b }) {
-  const color = `rgb(${r}, ${g}, ${b})`;
+function createMaterial(color) {
   if (materialCache[color]) {
     return materialCache[color];
   }
@@ -59,45 +61,50 @@ function createMesh(geometry, material) {
   }
 }
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const geometryCache = {};
 
-function createCube({ x, y, z, r, g, b }, dx, dy, dz) {
-  const material = createMaterial({ r, g, b });
-  const mesh = createMesh(boxGeometry, material);
-  mesh.position.set(x + dx, y + dy, z + dz);
-  return mesh;
+function createGeometry(width, height) {
+  if (geometryCache[width] === undefined) {
+    geometryCache[width] = {}
+  }
+
+  if (geometryCache[width][height] === undefined) {
+    geometryCache[width][height] = new THREE.PlaneBufferGeometry(width, height)
+  }
+
+  return geometryCache[width][height];
 }
 
-const planeGeometry = new THREE.PlaneBufferGeometry(1, 1);
 const quarterTurn = Math.PI / 2;
 const halfTurn = Math.PI;
 
-function createFace({ x, y, z, r, g, b }, dx, dy, dz, side) {
-  const material = createMaterial({ r, g, b });
-  const mesh = createMesh(planeGeometry, material);
+function createFace({ x, y, z, color, width, height }, dx, dy, dz, side) {
+  const material = createMaterial(color);
+  const geometry = createGeometry(width, height);
+  const mesh = createMesh(geometry, material);
   switch (side) {
     case "left":
       mesh.rotateY(-quarterTurn);
-      mesh.position.set(x + dx - 0.5, y + dy, z + dz);
+      mesh.position.set(x + dx - 0.5, y + dy + (0.5 * height) - 0.5, z + dz + (0.5 * width) - 0.5);
       break;
     case "right":
       mesh.rotateY(quarterTurn);
-      mesh.position.set(x + dx + 0.5, y + dy, z + dz);
+      mesh.position.set(x + dx + 0.5, y + dy + (0.5 * height) - 0.5, z + dz + (0.5 * width) - 0.5);
       break;
     case "bottom":
       mesh.rotateX(quarterTurn);
-      mesh.position.set(x + dx, y + dy - 0.5, z + dz);
+      mesh.position.set(x + dx + (0.5 * width) - 0.5, y + dy - 0.5, z + dz + (0.5 * height) - 0.5);
       break;
     case "top":
       mesh.rotateX(-quarterTurn);
-      mesh.position.set(x + dx, y + dy + 0.5, z + dz);
+      mesh.position.set(x + dx + (0.5 * width) - 0.5, y + dy + 0.5, z + dz + (0.5 * height) - 0.5);
       break;
     case "back":
       mesh.rotateX(halfTurn);
-      mesh.position.set(x + dx, y + dy, z + dz - 0.5);
+      mesh.position.set(x + dx + (0.5 * width) - 0.5, y + dy + (0.5 * height) - 0.5, z + dz - 0.5);
       break;
     case "front":
-    mesh.position.set(x + dx, y + dy, z + dz + 0.5);
+      mesh.position.set(x + dx + (0.5 * width) - 0.5, y + dy + (0.5 * height) - 0.5, z + dz + 0.5);
       break;
   }
   return mesh;
@@ -123,12 +130,12 @@ function createModelAt(model, scene, x, y, z) {
     });
   }
 
-  createSides(model.leftVisible, "left");
-  createSides(model.rightVisible, "right");
-  createSides(model.topVisible, "top");
-  createSides(model.bottomVisible, "bottom");
-  createSides(model.frontVisible, "front");
-  createSides(model.backVisible, "back");
+  createSides(model.leftFaces, "left");
+  createSides(model.rightFaces, "right");
+  createSides(model.topFaces, "top");
+  createSides(model.bottomFaces, "bottom");
+  createSides(model.frontFaces, "front");
+  createSides(model.backFaces, "back");
 }
 
 Promise.all(
@@ -168,6 +175,18 @@ Promise.all(
   animate();
 });
 
+function create2dArray(size1, size2, value) {
+  const array = [];
+  for (let j = 0; j < size1; j++) {
+    const row = [];
+    for (let k = 0; k < size2; k++) {
+      row.push(value);
+    }
+    array.push(row);
+  }
+  return array;
+}
+
 function loadModel(name) {
   const path = `./src/models/${name}.png`;
 
@@ -206,12 +225,20 @@ function loadModel(name) {
       size,
       voxels: [],
       visible: [],
+
       topVisible: [],
       bottomVisible: [],
       leftVisible: [],
       rightVisible: [],
       backVisible: [],
-      frontVisible: []
+      frontVisible: [],
+
+      topFaces: [],
+      bottomFaces: [],
+      leftFaces: [],
+      rightFaces: [],
+      backFaces: [],
+      frontFaces: []
     };
 
     const partsPerPixel = 4;
@@ -226,7 +253,7 @@ function loadModel(name) {
           );
 
           if (a !== 0) {
-            model.voxels.push({ x, y: 16 - y, z, r, g, b });
+            model.voxels.push({ x, y: size - (y + 1), z, r, g, b });
           }
         }
       }
@@ -234,11 +261,13 @@ function loadModel(name) {
 
     console.timeEnd(`${path}: reading`);
 
-    console.time(`${path}: optimising`);
+    console.time(`${path}: finding visible faces`);
 
     function isVoxel(x1, y1, z1) {
       return model.voxels.some(({ x, y, z }) => x1 == x && y1 == y && z1 == z);
     }
+
+    // TODO: Edges will always be visible.
 
     model.voxels.forEach(voxel => {
       const { x, y, z } = voxel;
@@ -285,10 +314,283 @@ function loadModel(name) {
       }
     });
 
-    console.timeEnd(`${path}: optimising`);
+    console.timeEnd(`${path}: finding visible faces`);
 
-    console.log(`${path}: ${model.voxels.length} total voxels`);
-    console.log(`${path}: ${model.visible.length} visible voxels`);
+    console.time(`${path}: combining visible faces`);
+
+    function getVoxel(voxels, x1, y1, z1) {
+      const matches = voxels.filter(
+        ({ x, y, z }) => x1 == x && y1 == y && z1 == z
+      );
+      return matches.length > 0 ? matches[0] : undefined;
+    }
+
+    // Tops/bottoms
+    for (let y = 0; y < size; y++) {
+      const topMask = create2dArray(size, size, "");
+      const bottomMask = create2dArray(size, size, "");
+      for (let x = 0; x < size; x++) {
+        for (let z = 0; z < size; z++) {
+          const topVoxel = getVoxel(model.topVisible, x, y, z);
+          if (topVoxel) {
+            topMask[x][z] = createColor(topVoxel);
+          }
+
+          const bottomVoxel = getVoxel(model.bottomVisible, x, y, z);
+          if (bottomVoxel) {
+            bottomMask[x][z] = createColor(bottomVoxel);
+          }
+        }
+      }
+
+      function combineFaces(mask, target) {
+        for (let x = 0; x < size; x++) {
+          for (let z = 0; z < size; z++) {
+            // Starting point
+            const color = mask[x][z];
+            if (color !== "") {
+              mask[x][z] = "";
+              let width = 1;
+              let canExpandWidth = true;
+              while (canExpandWidth) {
+                const nextWidth = width + 1;
+                const xToCheck = x + nextWidth - 1;
+
+                if (xToCheck >= size) {
+                  canExpandWidth = false;
+                } else if (mask[xToCheck][z] === color) {
+                  width = nextWidth;
+                  mask[xToCheck][z] = "";
+                } else {
+                  canExpandWidth = false;
+                }
+              }
+
+              let depth = 1;
+              let canExpandDepth = true;
+              while (canExpandDepth) {
+                const nextDepth = depth + 1;
+                const zToCheck = z + nextDepth - 1;
+
+                if (zToCheck >= size) {
+                  canExpandDepth = false;
+                } else {
+                  let nextRowMatches = true;
+                  for (let i = 0; i < width; i++) {
+                    if (mask[x + i][zToCheck] !== color) {
+                      nextRowMatches = false;
+                    }
+                  }
+
+                  if (nextRowMatches) {
+                    depth = nextDepth;
+                    for (let i = 0; i < width; i++) {
+                      mask[x + i][zToCheck] = "";
+                    }
+                  } else {
+                    canExpandDepth = false;
+                  }
+                }
+              }
+
+              target.push({
+                x,
+                y,
+                z,
+                width,
+                height: depth,
+                color
+              });
+            }
+          }
+        }
+      }
+
+      combineFaces(topMask, model.topFaces);
+      combineFaces(bottomMask, model.bottomFaces);
+    }
+
+    // Lefts/rights
+    for (let x = 0; x < size; x++) {
+      const leftMask = create2dArray(size, size, "");
+      const rightMask = create2dArray(size, size, "");
+      for (let y = 0; y < size; y++) {
+        for (let z = 0; z < size; z++) {
+          const leftVoxel = getVoxel(model.leftVisible, x, y, z);
+          if (leftVoxel) {
+            leftMask[y][z] = createColor(leftVoxel);
+          }
+
+          const rightVoxel = getVoxel(model.rightVisible, x, y, z);
+          if (rightVoxel) {
+            rightMask[y][z] = createColor(rightVoxel);
+          }
+        }
+      }
+
+      function combineFaces(mask, target) {
+        for (let y = 0; y < size; y++) {
+          for (let z = 0; z < size; z++) {
+            // Starting point
+            const color = mask[y][z];
+            if (color !== "") {
+              mask[y][z] = "";
+              let height = 1;
+              let canExpandHeight = true;
+              while (canExpandHeight) {
+                const nextHeight = height + 1;
+                const yToCheck = y + nextHeight - 1;
+
+                if (yToCheck >= size) {
+                  canExpandHeight = false;
+                } else if (mask[yToCheck][z] === color) {
+                  height = nextHeight;
+                  mask[yToCheck][z] = "";
+                } else {
+                  canExpandHeight = false;
+                }
+              }
+
+              let depth = 1;
+              let canExpandDepth = true;
+              while (canExpandDepth) {
+                const nextDepth = depth + 1;
+                const zToCheck = z + nextDepth - 1;
+
+                if (zToCheck >= size) {
+                  canExpandDepth = false;
+                } else {
+                  let nextRowMatches = true;
+                  for (let i = 0; i < height; i++) {
+                    if (mask[y + i][zToCheck] !== color) {
+                      nextRowMatches = false;
+                    }
+                  }
+
+                  if (nextRowMatches) {
+                    depth = nextDepth;
+                    for (let i = 0; i < height; i++) {
+                      mask[y + i][zToCheck] = "";
+                    }
+                  } else {
+                    canExpandDepth = false;
+                  }
+                }
+              }
+
+              target.push({
+                x,
+                y,
+                z,
+                width: depth,
+                height,
+                color
+              });
+            }
+          }
+        }
+      }
+
+      combineFaces(leftMask, model.leftFaces);
+      combineFaces(rightMask, model.rightFaces);
+    }
+
+    // Fronts/backs
+    for (let z = 0; z < size; z++) {
+      const frontMask = create2dArray(size, size, "");
+      const backMask = create2dArray(size, size, "");
+      for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
+          const frontVoxel = getVoxel(model.frontVisible, x, y, z);
+          if (frontVoxel) {
+            frontMask[x][y] = createColor(frontVoxel);
+          }
+
+          const backVoxel = getVoxel(model.backVisible, x, y, z);
+          if (backVoxel) {
+            backMask[x][y] = createColor(backVoxel);
+          }
+        }
+      }
+
+      function combineFaces(mask, target) {
+        for (let x = 0; x < size; x++) {
+          for (let y = 0; y < size; y++) {
+            // Starting point
+            const color = mask[x][y];
+            if (color !== "") {
+              mask[x][y] = "";
+              let width = 1;
+              let canExpandWidth = true;
+              while (canExpandWidth) {
+                const nextWidth = width + 1;
+                const xToCheck = x + nextWidth - 1;
+
+                if (xToCheck >= size) {
+                  canExpandWidth = false;
+                } else if (mask[xToCheck][y] === color) {
+                  width = nextWidth;
+                  mask[xToCheck][y] = "";
+                } else {
+                  canExpandWidth = false;
+                }
+              }
+
+              let height = 1;
+              let canExpandHeight = true;
+              while (canExpandHeight) {
+                const nextHeight = height + 1;
+                const yToCheck = y + nextHeight - 1;
+
+                if (yToCheck >= size) {
+                  canExpandHeight = false;
+                } else {
+                  let nextRowMatches = true;
+                  for (let i = 0; i < width; i++) {
+                    if (mask[x + i][yToCheck] !== color) {
+                      nextRowMatches = false;
+                    }
+                  }
+
+                  if (nextRowMatches) {
+                    height = nextHeight;
+                    for (let i = 0; i < width; i++) {
+                      mask[x + i][yToCheck] = "";
+                    }
+                  } else {
+                    canExpandHeight = false;
+                  }
+                }
+              }
+
+              target.push({
+                x,
+                y,
+                z,
+                width,
+                height,
+                color
+              });
+            }
+          }
+        }
+      }
+
+      combineFaces(frontMask, model.frontFaces);
+      combineFaces(backMask, model.backFaces);
+    }
+
+    console.timeEnd(`${path}: combining visible faces`);
+
+    console.log(
+      `${model.voxels.length} voxels (${model.voxels.length *
+        6} faces) compressed to ${model.backFaces.length +
+        model.bottomFaces.length +
+        model.frontFaces.length +
+        model.leftFaces.length +
+        model.rightFaces.length +
+        model.topFaces.length} faces`
+    );
 
     return model;
   });
